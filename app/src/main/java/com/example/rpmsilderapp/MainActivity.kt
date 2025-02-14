eef2d3f4184b88ec360291127e878d6240b418f7
aiden cox <cox.378@wright.edu>
2025-02-14 12:55:24 -0500

    added slider position using remember and mutable state of key words

--- app/src/main/java/com/example/rpmsilderapp/MainActivity.kt
+++ app/src/main/java/com/example/rpmsilderapp/MainActivity.kt
@@ -12,8 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -53,6 +58,16 @@ fun RPMSlider(
     /* parameters? */
     modifier: Modifier = Modifier
 ) {
+    var sliderPosition by remember { mutableStateOf(0f) }
+        Column(modifier = modifier){
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 0f..1f,
+            )
+            Text(text = sliderPosition.toString())
+        }
+
     // the slider code will go here
 
 }
